50f71923c783576dd9a481c316dda51fece30049
Nicolas Epstein <nilueps@gmail.com>
2024-05-13 09:02:13 -0400

    fixups

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("services/", views.services, name="services"),
-    path("technologies/", views.tech, name="tech"),
-    path("about/", views.about, name="about"),
-    # path("contact/", views.contact, name="contact"),
 ]
