ab3c8aaa9c40afffe36b019fff57d82e26db061b
Adam <arbobeck@gmail.com>
2024-05-14 21:17:25 +0200

    fix model errors in person.py

--- app/models/person.py
+++ app/models/person.py
@@ -50,7 +50,7 @@ class AbstractPerson(models.Model):
     native_name_lang = models.CharField(
         _("language of native name"),
         choices=settings.LANGUAGES,
-        max_length=5,
+        max_length=10,
         blank=True,
     )
     pronunciation = models.CharField(
@@ -105,6 +105,7 @@ class AbstractPerson(models.Model):
         },
         on_delete=models.SET_NULL,
         null=True,
+        related_name='person_birth_places',
     )
     birth_place_object_id = models.PositiveIntegerField()
     birth_place = GenericForeignKey(
@@ -124,10 +125,11 @@ class AbstractPerson(models.Model):
         },
         on_delete=models.SET_NULL,
         null=True,
+        related_name='person_disappearance_places',
     )
     disappearance_place_object_id = models.PositiveIntegerField()
     disappearance_place = GenericForeignKey(
-        "appearance_place_content_type", "disappearance_place_object_id"
+        "disappearance_place_content_type", "disappearance_place_object_id"
     )
 
     death_date = models.DateField(_("date of death"), null=True, blank=True)
@@ -138,6 +140,7 @@ class AbstractPerson(models.Model):
         },
         on_delete=models.SET_NULL,
         null=True,
+        related_name='person_death_places'
     )
     death_place_object_id = models.PositiveIntegerField()
     death_place = GenericForeignKey(
@@ -223,14 +226,14 @@ class Person(AbstractPerson):
     genetic_mother = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
-        related_name="genetic_children",
+        related_name='mother_of_child',
         null=True,
         blank=True,
     )
     genetic_father = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
-        related_name="genetic_children",
+        related_name='father_of_child',
         null=True,
         blank=True,
     )
