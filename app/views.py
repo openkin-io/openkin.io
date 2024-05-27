fecf67983bf7411c81e958059f73fcc1c520dc38
Adam <arbobeck@gmail.com>
2024-05-27 18:51:24 +0200

    update views.py and urls.py

--- app/views.py
+++ app/views.py
@@ -4,3 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 def index(request):
     return render(request, "pages/index.html", {"page_title": _("Home")})
+def about(request):
+    return render(request, "pages/about.html", {"page_title": _("About")})
