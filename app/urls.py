5530f6784b978f5f918b5f838ea26dbacec7fcbd
Adam <arbobeck@gmail.com>
2024-06-01 20:22:17 +0200

    style navbar, footer, and finish page links

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
-    path("familytree/", views.familytree, name="familytree")
+    path("familytree/", views.familytree, name="familytree"),
+    path("faq/", views.faq, name="faq"),
+    path("learn/", views.learn, name="learn"),
+    path("privacy/", views.privacy, name="privacy"),
+    path("terms/", views.terms, name="terms"),
 ]
--- app/views.py
+++ app/views.py
@@ -10,3 +10,11 @@ def contact(request):
     return render(request, "pages/contact.html", {"page_title": _("Contact")})
 def familytree(request):
     return render(request, "pages/familytree.html", {"page_title": _("Family Tree")})
+def faq(request):
+    return render(request, "pages/faq.html", {"page_title": _("FAQ")})
+def learn(request):
+    return render(request, "pages/learn.html", {"page_title": _("Learn")})
+def privacy(request):
+    return render(request, "pages/privacy.html", {"page_title": _("Privacy")})
+def terms(request):
+    return render(request, "pages/terms.html", {"page_title": _("Terms")})
